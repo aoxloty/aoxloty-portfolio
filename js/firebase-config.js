4bad60e63159a68fabe9061320aa8137f712ace7
aoxloty <84072063+aoxloty@users.noreply.github.com>
2026-01-13 21:25:20 +0700

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+// File này chứa cấu hình Firebase
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyALTrutr6VsBSPyv0RDVSnuZ1by8HTD_Hs",
+  authDomain: "aoxloty-ab7b3.firebaseapp.com",
+  databaseURL: "https://aoxloty-ab7b3-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "aoxloty-ab7b3",
+  storageBucket: "aoxloty-ab7b3.firebasestorage.app",
+  messagingSenderId: "656551781954",
+  appId: "1:656551781954:web:520ad183ddd5de7f476fdb"
+};
